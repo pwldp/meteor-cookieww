f99c03f03c45579e1a2316e151cafe6d7b1eee90
pwldp <pawlakdp@gmail.com>
2013-12-16 15:49:52 +0100

    Create smart.js

---
+++ smart.js
@@ -0,0 +1,8 @@
+{
+        "name": "cookieww",
+        "description": "Avatec - Cookie Warning Window packaged for Meteor",
+        "homepage": "https://github.com/pwldp/meteor-cookieww",
+        "author": "Dariusz Pawlak",
+        "version": "0.0.1",
+        "git": "https://github.com/pwldp/meteor-cookieww.git"
+}
